32a508beb1c01f81235f82ad014b3e96bed2233e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-08 17:13:21 +0200

    Created the definitions for animal class

---
+++ ex00/Animal.hpp
@@ -0,0 +1,18 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+protected:
+	std::string _type;
+public:
+	Animal(void);
+	Animal(const Animal &toCopy);
+	Animal &operator=(const Animal &toCopy);
+	virtual ~Animal(void);
+	virtual void makeSound(void) const;
+};
+#endif
