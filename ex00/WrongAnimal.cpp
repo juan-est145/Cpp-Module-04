ecc591087be77326423f4a6bdda14cf07af5c03b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-09 11:54:08 +0200

    Implemented the WrongAnimal class

--- ex00/WrongAnimal.cpp
+++ ex00/WrongAnimal.cpp
@@ -1,13 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongAnimal.cpp                                    :+:      :+:    :+:   */
+/*   WrongWrongAnimal.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 11:51:18 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/09 11:51:22 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/09 11:53:12 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(void)
+{
+	std::cout << "The WrongAnimal default constructor was called" << std::endl;
+	this->_type = "WrongAnimal";
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &toCopy)
+{
+	std::cout << "The WrongAnimal copy constructor was called" << std::endl;
+	*this = toCopy;
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &toCopy)
+{
+	std::cout << "The WrongAnimal copy assignment operator was called" << std::endl;
+	if (this != &toCopy)
+		this->_type = toCopy._type;
+	return (*this);
+}
+
+void WrongAnimal::makeSound(void) const
+{
+	std::cout << "*Weird, undefined WrongAnimal noises*" << std::endl;
+}
+
+std::string WrongAnimal::getType(void) const
+{
+	return (this->_type);
+}
+
+WrongAnimal::~WrongAnimal(void)
+{
+	std::cout << "WrongAnimal destructor was called" << std::endl;
+}
