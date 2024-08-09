0dfd9b9fbd3b5ed73da266719a0957d31f4ee9ca
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-09 11:51:41 +0200

    Wrote definitions of WrongAnimal class

---
+++ ex00/WrongAnimal.hpp
@@ -0,0 +1,19 @@
+#ifndef WRONGANIMAL_HPP
+#define WRONGANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class WrongAnimal
+{
+protected:
+	std::string _type;
+public:
+	WrongAnimal(void);
+	WrongAnimal(const WrongAnimal &toCopy);
+	WrongAnimal &operator=(const WrongAnimal &toCopy);
+	void makeSound(void) const;
+	std::string getType(void) const;
+	~WrongAnimal(void);
+};
+#endif
