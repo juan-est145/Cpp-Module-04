2f134ca1d612ce6a2a9a152f5a5355526cccebba
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-13 18:48:41 +0200

    ex03 initial commit

---
+++ ex03/AMateria.hpp
@@ -0,0 +1,15 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <iostream>
+#include <string>
+
+class AMateria
+{
+private:
+	/* data */
+public:
+	AMateria(void);
+	~AMateria(void);
+};
+#endif
