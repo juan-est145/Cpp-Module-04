c0abdab45a0d95f43771b0ca0a0ca96f5a14e2c5
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-14 09:57:13 +0200

    Implemented most of AMateria class methods

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,8 +6,35 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 18:46:37 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/13 18:46:46 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/14 09:56:54 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+
+AMateria::AMateria(void)
+{
+	this->_type = "default";
+}
+
+AMateria::AMateria(std::string const &type)
+{
+	this->_type = type;
+}
+
+AMateria::AMateria(const AMateria &toCopy)
+{
+	*this = toCopy;
+}
+
+AMateria &AMateria::operator=(const AMateria &toCopy)
+{
+	if (this != &toCopy)
+		this->_type = toCopy._type;
+	return (*this);
+}
+
+std::string const &AMateria::getType() const
+{
+	return (this->_type);
+}
--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -1,15 +1,20 @@
 #ifndef AMATERIA_HPP
 #define AMATERIA_HPP
 
-#include <iostream>
 #include <string>
 
 class AMateria
 {
-private:
-	/* data */
+protected:
+	std::string _type;
 public:
 	AMateria(void);
-	~AMateria(void);
+	AMateria(std::string const &type);
+	AMateria(const AMateria &toCopy);
+	AMateria &operator=(const AMateria &toCopy);
+	std::string const &getType() const;
+	virtual AMateria *clone() const = 0;
+	//virtual void use(ICharacter &target); Character interface pending to be created 
+	virtual ~AMateria(void) = 0;
 };
 #endif
