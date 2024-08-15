9f321e564ef4791ff764801060934c96f0ed54a3
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-15 19:22:37 +0200

    Implemented getName and equip methods for Character

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 15:18:14 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/15 18:27:33 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/15 19:07:29 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void Character::_initInventory(void)
 {
-	unsigned int size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < this->_size; i++)
 		this->_inventory[i] = NULL;
 }
 
 void Character::_destroyInventory(void)
 {
-	unsigned int size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < this->_size; i++)
 	{
 		if (this->_inventory[i] != NULL)
 			delete this->_inventory[i];
@@ -31,18 +29,32 @@ void Character::_destroyInventory(void)
 
 void Character::_copyInventory(const Character &toCopy)
 {
-	unsigned int size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < this->_size; i++)
 			this->_inventory[i] = toCopy._inventory[i];	
+}
+
+void Character::_dropToGround(AMateria *toDrop)
+{
+	static unsigned int index = 0;
+	unsigned int size = (unsigned int)(sizeof(this->_ground) / sizeof(this->_ground[0]));
+
+	if (this->_ground[index] != NULL)
+		delete this->_ground[index];
+	this->_ground[index] = toDrop;
+	index = (index + 1) % size;
 }
 
 Character::Character(void) 
 {
+	this->_size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
+	this->_groundSize = (unsigned int)(sizeof(this->_ground) / sizeof(this->_ground[0]));
 	this->_initInventory();
 }
 
 Character::Character(std::string name)
 {
+	this->_size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
+	this->_groundSize = (unsigned int)(sizeof(this->_ground) / sizeof(this->_ground[0]));
 	this->_name = name;
 	this->_initInventory();
 	std::cout << "Character " << this->_name << " has been spawned into this cruel world" << std::endl;
@@ -50,9 +62,10 @@ Character::Character(std::string name)
 
 Character::Character(const Character &toCopy)
 {
-	unsigned int size = (unsigned int)(sizeof(this->_inventory) / sizeof(this->_inventory[0]));
+	this->_size = toCopy._size;
+	this->_groundSize = toCopy._groundSize;
 	this->_name = toCopy._name;
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < toCopy._size; i++)
 		this->_inventory[i] = toCopy._inventory[i];
 }
 
@@ -60,11 +73,44 @@ Character &Character::operator=(const Character &toCopy)
 {
 	if (this != &toCopy)
 	{
-		this->_destroyInventory();
+		this->_size = toCopy._size;
+		this->_groundSize = toCopy._groundSize;
 		this->_name = toCopy._name;
+		this->_destroyInventory();
 		this->_copyInventory(toCopy);
 	}
 	return (*this);
+}
+
+std::string const &Character::getName(void) const
+{
+	return (this->_name);
+}
+
+void Character::equip(AMateria *m)
+{
+	for (unsigned int i = 0; i < this->_size; i++)
+	{
+		if (this->_inventory[i] != NULL)
+		{
+			this->_inventory[i] = m;
+			std::cout << "Materia " << m->getType() << " has been stored at index " << i << std::endl;
+			return ;
+		}
+	}
+	std::cout << "Inventory is full, cannot save that materia" << std::endl;
+}
+
+void Character::unequip(int idx)
+{
+	if (idx < 0 || idx >= this->_size || this->_inventory[idx] == NULL)
+	{
+		std::cout << "Invalid index" << std::endl;
+		return ;
+	}
+	
+	std::cout << "Unequiped Materia " << this->_inventory[idx]->getType() << std::endl;
+	this->_inventory[idx] == NULL;
 }
 
 Character::~Character(void)
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -11,14 +11,22 @@ class Character : public ICharacter
 private:
 	std::string _name;
 	AMateria *_inventory[4];
+	AMateria *_ground[20];
+	unsigned int _size;
+	unsigned int _groundSize;
 	void _initInventory(void);
 	void _destroyInventory(void);
 	void _copyInventory(const Character &toCopy);
+	void _dropToGround(AMateria *toDrop);
 public:
 	Character(void);
 	Character(std::string name);
 	Character(const Character &toCopy);
 	Character &operator=(const Character &toCopy);
+	std::string const &getName(void) const;
+	void equip(AMateria *m);
+	void unequip(int idx);
+	void use(int idx, ICharacter &target);
 	~Character(void);
 };
 #endif
